f128452f5c245dd6bac78ff67a34c7f3998ccb43
Etil <149319300+Etilll@users.noreply.github.com>
2024-02-02 17:00:57 +0200

    Delete copium.py

--- CodeCrafters_assistant/CodeCrafters_assistant/copium.py
+++
@@ -1,9 +0,0 @@
-
-        # from flask import Flask
-        # app = Flask(__name__)
-
-        # @app.route('/')
-        # def hello():
-        #     return string
-        
-        # app.run(debug=False, host='0.0.0.0')
